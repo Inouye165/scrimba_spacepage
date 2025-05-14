b846873cd5e3d293f0362299cbc9f15f8810538f
inouye165 <inouye165@gmail.com>
2025-05-14 13:58:02 -0700

    feat: finished kinda

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-// document.getElementById("count").innerText = 5
-
-// let count = 5
-// count = count + 1
-// console.log(count)
-
-// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
-// Decrease it down to 25, and then finally increase it to 70
-// Console.log the value after each step
-
-let count = 0
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let count = 0
+
 function increment() {
-    console.log(count)
-    count = count + 1
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count
 }
-document.getElementById("count-el").innerText = count
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
